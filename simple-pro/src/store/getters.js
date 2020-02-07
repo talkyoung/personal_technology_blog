8687e38ec51062051bb3a0568ca070d63abe412d
1120655294@qq.com <Ty19980622>
2020-02-07 22:05:11 +0800

    统统删除 迎接新代码

--- simple-pro/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  token: state => state.user.token,
-  userId: state => state.user.userId,
-  isLoadRoutes: state => state.user.isLoadRoutes,
-  permissions: state => state.user.permissions,
-};
-export default getters
--- simple/sim-pro/src/main/java/com/talkyoung/simpro/exception/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.talkyoung.simpro.exception;
-
-public class UserNotFoundException extends RuntimeException {
-}
